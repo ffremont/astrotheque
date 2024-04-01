b61043dec8e25cccc484a3da88fb7d3a01b2f2f2
Florent FREMONT <ff.fremont.florent@gmail.com>
2024-04-01 19:48:44 +0200

    chore windows build

---
+++ platforms/windows/start.bat
@@ -0,0 +1,41 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFB1SShaHAE+1EbsQ5+n//NaQq0MZW/UsNY3O1LCCJdwS+VH2eoIl02tPnYYBDRhXbS6ibQA6/D8QiXaKIsmYtgusGnCg3kwcOU86jmDf7A==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB1SShaHAE+1EbsQ5+n//NaQq0MZW/UsNY3O1LCCJdwS+VH2eoIl02tPnYYBDRhXbS6KfRsmpnpKs3WQMomZqwqB
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+title Astrotheque
+
+set DB_FILE=core/dbs/astrotheque.db
+set HTML_DIST=core/dist
+core\jdk-21.0.2+13-jre\bin\java.exe -jar astrotheque.jar
+
+
+pause
---
+++ scripts/windows-build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.2%2B13/OpenJDK21U-jre_x64_windows_hotspot_21.0.2_13.zip
+
+cd target
+mkdir astrotheque-windows
+cp -r ../platforms/windows/x64/ astrotheque-windows/core/
+cp -r ../dist astrotheque-windows/core/
+cp -r ../dbs astrotheque-windows/core/
+cp astrotheque.jar astrotheque-windows/
+cp ../platforms/windows/Astrotheque.exe astrotheque-windows
+
+zip -vr astrotheque-windows-x64-bundle.zip astrotheque-windows -x "*.DS_Store"
+
+cd ..
